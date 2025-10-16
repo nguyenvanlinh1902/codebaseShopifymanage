777e6a1ba6d7f466de63d3b4f48c9379ff350c3b
thomasavada <anhnt@avada.io>
2025-10-16 13:46:57 +0700

    Handle CSP header

--- packages/functions/src/index.js
+++ packages/functions/src/index.js
@@ -3,6 +3,12 @@ import apiHandler from './handlers/api';
 import apiSaHandler from './handlers/apiSa';
 import authHandler from './handlers/auth';
 import authSaHandler from './handlers/authSa';
+import embedAppHandler from './handlers/embed';
+
+export const embedApp = functions
+  .runWith({memory: '256MB'})
+  .region('us-central1', 'us-east1', 'europe-west2', 'asia-northeast1')
+  .https.onRequest(embedAppHandler.callback());
 
 export const api = functions.https.onRequest(apiHandler.callback());
 export const apiSa = functions.https.onRequest(apiSaHandler.callback());

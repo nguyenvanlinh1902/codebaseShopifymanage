243c47e5b0a0c9d0c296e0137a0a030ebad702b3
thomasavada <thomas@avada.io>
2025-11-30 00:20:16 +0700

    Add logging

--- packages/functions/src/controllers/shopController.js
+++ packages/functions/src/controllers/shopController.js
@@ -10,7 +10,9 @@ import {logger} from 'firebase-functions/v2';
 export async function getUserShops(ctx) {
   try {
     const shopId = getCurrentShop(ctx);
+    logger.info('Get user shops', shopId);
     const [shop, shopInfo] = await Promise.all([getShopById(shopId), getShopInfoByShopId(shopId)]);
+    logger.info('Got shop info', shopInfo);
 
     ctx.body = {shop, shopInfo};
   } catch (e) {

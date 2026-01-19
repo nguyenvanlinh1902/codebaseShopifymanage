55088873a7854fd0d831c7aae9efcdc93a38ded3
DucMDao <ducdm@avadagroup.com>
2026-01-19 16:58:42 +0700

    abc

--- extensions/theme-extension/assets/avada-embed.js
+++ extensions/theme-extension/assets/avada-embed.js
@@ -1,5 +1,5 @@
 (function() {
-  const BASE_URL = 'https://jacob-smart-ear-oliver.trycloudflare.com/scripttag';
+  const BASE_URL = 'https://class-fought-rides-afternoon.trycloudflare.com/scripttag';
 
   const scriptElement = document.createElement('script');
   scriptElement.type = 'text/javascript';

2a58a265b2b675f5048750bb0d3b977803be0d34
linhnv <linhnv@avadagroup.com>
2026-02-10 13:28:19 +0700

    fix embed app

--- packages/assets/src/layouts/EmbeddedLayout.js
+++ packages/assets/src/layouts/EmbeddedLayout.js
@@ -1,23 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Frame} from '@shopify/polaris';
-import {NavMenu} from '@shopify/app-bridge-react';
-import {NavLink} from 'react-router-dom';
 
 export default function EmbeddedLayout({children}) {
-  return (
-    <>
-      <NavMenu>
-        <NavLink to="/" rel="home">Dashboard</NavLink>
-        <NavLink to="/products">Products Import</NavLink>
-        <NavLink to="/orders">Orders & Sheets</NavLink>
-        <NavLink to="/settings">Settings</NavLink>
-      </NavMenu>
-      <Frame>
-        {children}
-      </Frame>
-    </>
-  );
+  return <Frame>{children}</Frame>;
 }
 
 EmbeddedLayout.propTypes = {
